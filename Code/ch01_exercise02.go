b707643c014f1a79c08f529f908ea61e68fded82
Takahiko Okada <takahikookada@gmail.com>
2018-08-09 20:53:58 +0200

    ch01 exercise 2 done

---
+++ Code/ch01_exercise02.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "strconv"
+)
+
+func main() {
+  s, sep := "", " "
+  for i, arg := range os.Args[1:] {
+    s = strconv.Itoa(i) + sep + arg
+    fmt.Println(s)
+  }
+}
+
+// convert int using Itoa func in strconv package
